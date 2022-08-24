3d64d55fd05feb6da9d407798c35773086526eba
Richard Shea <rshea@thecubagroup.com>
2022-08-24 22:29:19 +1200

    Refactor code so that it's easier to see what's going on. Put processing specific to initialization into a dedicated module

---
+++ src/DataInitializationUtilities.js
@@ -0,0 +1,55 @@
+import { Input, Select } from 'react-spreadsheet-grid'
+
+const somePositions = [
+    {id: 'position1', name: 'Bus Driver'} , 
+    {id: 'position2', name: 'Doctor'} , 
+    {id: 'position3', name: 'Tax Inspector'} , 
+    {id: 'position4', name: 'House Painter'}
+];
+const rowData = [
+    { id: 'user1', name: 'Jane Lee', age: 22, positionId: 'position1' },
+    { id: 'user2', name: 'John Smith', age: 33, positionId: 'position1' },
+    { id: 'user3', name: 'Ranjit Singh', age: 44, positionId: 'position1' },
+    { id: 'user4', name: 'Annabelle Fuentes', age: 55, positionId: 'position1' },
+];
+
+
+const initColumns = () => {
+    return [
+    {
+      title: () => 'Name',
+      value: (row, { focus }) => {
+          return (
+              <Input
+                value={row.name}
+                focus={focus}
+              />
+          );
+      }
+    }, 
+    {
+      title: () => 'Age',
+      value: (row, { focus }) => {
+          return (
+              <Input
+                value={row.age}
+                focus={focus}
+              />
+          );
+      }
+    }, 
+    {
+      title: () => 'Position',
+      value: (row, { focus }) => {
+          return (
+              <Select
+                value={row.positionId}
+                isOpen={focus}
+                items={somePositions}
+              />
+          );
+      }
+    }
+  ]
+}
+export { initColumns, rowData } 
--- src/components/myawesomegrid.js
+++ src/components/myawesomegrid.js
@@ -1,58 +1,16 @@
-import React   from 'react'
-import { Grid, Input, Select } from 'react-spreadsheet-grid'
-
-const rows = [
-    { id: 'user1', name: 'Jane Lee', age: 22, positionId: 'position1' },
-    { id: 'user2', name: 'John Smith', age: 33, positionId: 'position1' },
-    { id: 'user3', name: 'Ranjit Singh', age: 44, positionId: 'position1' },
-    { id: 'user4', name: 'Annabelle Fuentes', age: 55, positionId: 'position1' },
-];
-
-const somePositions = [
-    {id: 'position1', name: 'Bus Driver'} , 
-    {id: 'position2', name: 'Doctor'} , 
-    {id: 'position3', name: 'Tax Inspector'} , 
-    {id: 'position4', name: 'House Painter'}
-];
+import React from 'react'
+import { useState } from 'react';
+import { Grid } from 'react-spreadsheet-grid'
+import { initColumns, rowData } from '../DataInitializationUtilities'
 
 const MyAwesomeGrid = () => {
+  // eslint-disable-next-line
+  const [columns, setColumns] = useState(initColumns()) 
+
   return (
     <Grid
-      columns={[
-        {
-          title: () => 'Name',
-          value: (row, { focus }) => {
-              return (
-                  <Input
-                    value={row.name}
-                    focus={focus}
-                  />
-              );
-          }
-        }, {
-          title: () => 'Age',
-          value: (row, { focus }) => {
-              return (
-                  <Input
-                    value={row.age}
-                    focus={focus}
-                  />
-              );
-          }
-        }, {
-          title: () => 'Position',
-          value: (row, { focus }) => {
-              return (
-                  <Select
-                    value={row.positionId}
-                    isOpen={focus}
-                    items={somePositions}
-                  />
-              );
-          }
-        }
-      ]}
-      rows={rows}
+      columns={columns}
+      rows={rowData}
       getRowKey={row => row.id}
     />
   )
